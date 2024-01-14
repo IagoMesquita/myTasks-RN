cf2cbebc4cc4de0d2411f515566da6b4be3af28f
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-01-14 17:44:46 -0300

    feat: add TasksProvider

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import {Home} from './src/pages/Home';
+import {TasksProvider} from './src/data/Context/TasksContext';
 
 export default function App() {
-  return <Home />;
+  return (
+    <TasksProvider>
+      <Home />
+    </TasksProvider>
+  );
 }
