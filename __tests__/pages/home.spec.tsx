236513ece0020dcd60f1bd965a3dc0c41d4149a3
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-01-22 07:09:52 -0300

    test: comoment Home

---
+++ __tests__/pages/home.spec.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import {Home} from '../../src/pages/Home';
+
+describe('Home component', () => {
+  it('renders correctly', () => {
+    const {getByPlaceholderText} = render(<Home />);
+
+    const inputNewTask = getByPlaceholderText('Nova tarefa...');
+
+    expect(inputNewTask).toBeDefined();
+    expect(inputNewTask.props.placeholder).toBeTruthy();
+  });
+});
