b0728671757eab038e1b8279e88248df160f05c1
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-01-22 20:43:40 -0300

    chore: config for coverage tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: 'react-native',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.tsx",
+    "!src/**/*.spect.tsx"
+  ],
+  coverageReporters: ["lcov"]
 };
